b9ffdbfafc118ca57fce910f0f568986ba97d9f1
[Gurudev kini] <[gurudevkinichess@gmail.com]>
2026-02-09 09:29:26 +0530

    basic file

---
+++ DAY_06/hosptil.java
@@ -0,0 +1,14 @@
+public class hosptil 
+{
+
+  private class patient 
+  {
+    String patientId;
+    String name;
+    int age;
+  }
+public static void main(String[] args) {
+  System.out.println(patient);
+  
+}
+}
---
+++ DAY_06/moveticket.java
@@ -0,0 +1,7 @@
+public class moveticket {
+  public static void main (String [] args)
+  
+  {
+    
+  }
+}
